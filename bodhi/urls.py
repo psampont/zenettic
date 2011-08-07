2de7d6ffe22a9a5f4e5ca6e2995e2c2380fe64a9
Philippe SAMPONT <sampont@gmail.com>
2011-08-07 10:56:13 +0200

    Add urls files

--- bodhi/urls.py
+++ bodhi/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls.defaults import *
-from bodhi.views import *
 
-urlpatterns = patterns('',
-    (r'^$', index),
+urlpatterns = patterns('bodhi.views',
+    (r'^$', 'index'),
+    (r'^(\d+)/$', 'device'),
+    (r'^(\d+)/ping$', 'device_ping'),
+    (r'^(\d+)/wol$', 'device_wol'),
+    (r'^(\d+)/pass$', 'device_ask_pass'),
+    (r'^(\d+)/shutdown$', 'device_shutdown'),
 )
--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from settings import DEBUG
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,10 +9,21 @@ urlpatterns = patterns('',
     # Example:
     (r'^bodhi/', include('zenettic.bodhi.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
+
+if DEBUG:
+    urlpatterns += patterns('',
+        (r'^styles/(?P<path>.*)$', 'django.views.static.serve',
+         {'document_root': 'c:/Users/psa/Projects/zenettic/templates/styles/'}),
+#         {'document_root': '/home/psa/Projects/zenettic/templates/styles/'}),
+        (r'^pix/(?P<path>.*)$', 'django.views.static.serve',
+         {'document_root': 'c:/Users/psa/Projects/zenettic/templates/pix/'}),
+#         {'document_root': '/home/psa/Projects/zenettic/templates/pix/'}),
+    )
+
