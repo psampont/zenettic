c14cb85c5ee8001237045e3374a6a6694a983571
Philippe SAMPONT <sampont@gmail.com>
2011-08-08 14:28:12 +0200

    Replace zenttic.sqlite3 file by auto-generated database

---
+++ bodhi/sql.sample/history.sql
@@ -0,0 +1,3 @@
+INSERT INTO "bodhi_history" VALUES(1,1,'2011-03-16 16:53:07.466434',0,1,'');
+INSERT INTO "bodhi_history" VALUES(2,2,'2011-03-16 17:00:32.049214',0,1,'');
+INSERT INTO "bodhi_history" VALUES(3,3,'2011-03-16 17:02:58.210063',0,0,'');
